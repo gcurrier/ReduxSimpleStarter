93b8ed1a4a08bb9bc5a4aa5a0a4f05774eddf57d
glen <jinx3y@gmail.com>
2018-02-05 23:24:28 +0100

    Section 2 tutorial completed; Ajax Requests using react; lodash library imported; search bar, video player, video list added.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import YT_KEY from './youtube_key';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+import YTKey from './youtube_key'; //you will need your own youtube key
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('UFO 2018')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: YTKey, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+
+    //throttle the function (slow it down)
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)
+    }, 500);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
